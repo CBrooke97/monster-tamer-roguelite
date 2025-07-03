a358b6407d5347b2fb4daae9174433049e650da3
Carter Brooke <carterbrooke@ntlworld.com>
2025-07-03 20:55:31 +0100

    Added missed code.

--- Scripts/AttributeComponent.cs
+++ Scripts/AttributeComponent.cs
@@ -71,23 +71,15 @@ public partial class AttributeComponent : Node
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		
-	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
+		foreach (EAttributeType attributeType in BaseAttributeMap.Keys)
+		{
+			AttributeModifiers.Add(attributeType, new());
+		}
 	}
 
 	public void OnMonsterDefinitionChanged(MonsterDefinitionResource newDefinition)
 	{
 		BaseAttributeMap = new Godot.Collections.Dictionary<EAttributeType, float>(newDefinition.AttributeMap);
-
-		BonusAttributeMap.Clear();
-		foreach(EAttributeType attribute in BaseAttributeMap.Keys)
-		{
-			BonusAttributeMap.Add(attribute, 0.0f);
-		}
 
 		if (_healthComponent != null)
 		{
